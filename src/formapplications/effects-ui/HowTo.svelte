45b6a9dc3440892058825a27f290fbde901d5847
Haxxer <adamoresten@gmail.com>
2023-04-24 12:38:07 +0100

    Fixed open settings button

--- src/formapplications/effects-ui/HowTo.svelte
+++ src/formapplications/effects-ui/HowTo.svelte
@@ -1,5 +1,29 @@
 <script>
   import { localize } from '@typhonjs-fvtt/runtime/svelte/helper';
+  import * as lib from "/lib/lib.js"
+
+  async function openSettings(){
+
+    const settings = new SettingsConfig();
+
+    await settings.render(true)
+
+    await lib.wait(75);
+
+    const focusElement = settings.element.find('select[name="sequencer.permissions-effect-create"]');
+
+    settings.element.find("div.scrollable").scrollTop(focusElement.offset().top)
+
+    await lib.wait(250);
+
+    focusElement.css("box-shadow", "rgba(200, 64, 67, 0.75) inset 0 0 10px");
+
+    await lib.wait(5000);
+
+    focusElement.css("box-shadow", "none");
+
+  }
+
 </script>
 
 <div class="howto-container">
@@ -7,7 +31,7 @@
 	<p>{@html localize("SEQUENCER.HowTo.Explanation")}</p>
 	{#if game.user.isGM}
 		<p>{@html localize("SEQUENCER.HowTo.PermissionsExplanation")}</p>
-		<button type="button" class="w-100 open-module-settings">
+		<button type="button" class="w-100 open-module-settings" on:click={openSettings}>
 			<i class="fas fa-plug"></i> {@html localize("SEQUENCER.HowTo.OpenModuleSettings")}
 		</button>
 	{/if}
