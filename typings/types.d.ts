7f6d59699da0edc46642a9485b27ba213f5627f3
MrVauxs <mrvauxs@gmail.com>
2024-09-27 08:59:25 +0200

    Update types.d.ts

--- typings/types.d.ts
+++ typings/types.d.ts
@@ -1421,6 +1421,7 @@ declare abstract class SequencerPresets {
 }
 
 declare namespace Sequencer {
+  const BaseSection: Section
   const Database: SequencerDatabase;
   const Presets: SequencerPresets;
   const Preloader: SequencerPreloader;
