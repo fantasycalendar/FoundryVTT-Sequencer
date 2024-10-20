307ceb2bd285fb06db487348fd7821dff40d2628
Arne Link <mail@arne-link.de>
2024-10-20 21:40:12 +0200

    Add content-type and content-length to cached spritesheet responses

--- src/lib/spritesheets/ktx2FileCache.js
+++ src/lib/spritesheets/ktx2FileCache.js
@@ -1,6 +1,10 @@
 // @ts-check
 
-const cacheVersion = 1;
+
+// cache version history:
+// 1: initial
+// 2: zstandard compressed ktx2 files and response metadata
+const cacheVersion = 2;
 const cacheName = `SequencerKTX2Cache-${cacheVersion}`;
 /** @type {Promise<Cache | null>} */
 let ktx2FileCache;
@@ -32,7 +36,7 @@ export class Ktx2FileCache {
 	 * @returns {Request}
 	 */
 	#getKtxFileCacheRequest(id, hash) {
-		return new Request(`${id}?spritesheet`, { headers: { "x-hash": hash } });
+		return new Request(`${id}?spritesheet`);
 	}
 
 	/**
@@ -87,7 +91,13 @@ export class Ktx2FileCache {
 			return;
 		}
 		const request = this.#getKtxFileCacheRequest(id, sourceHash);
-		const response = new Response(ktx2FileBuffer.buffer,  { headers: { "x-hash": sourceHash } });
+		const response = new Response(ktx2FileBuffer.buffer, {
+			headers: {
+				"x-hash": sourceHash,
+				"Content-Type": "image/ktx2",
+				"Content-Length": String(ktx2FileBuffer.byteLength),
+			},
+		});
 		await cacheStorage.put(request, response);
 	}
 
@@ -133,7 +143,14 @@ export class Ktx2FileCache {
 			return;
 		}
 		const request = this.#getSpriteDataCacheRequest(id, sourceHash);
-		const response = new Response(JSON.stringify(spritesData), { headers: { "x-hash": sourceHash } });
+		const spriteDataString = JSON.stringify(spritesData);
+		const response = new Response(spriteDataString, {
+			headers: {
+				"x-hash": sourceHash,
+				"Content-Type": "application/json",
+				"Content-Length": String(spriteDataString.length),
+			},
+		});
 		await cacheStorage.put(request, response);
 	}
 }
